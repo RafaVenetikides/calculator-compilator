d3b1e9b0cca4e56bdec8e1d3dc590ed504f36016
maturidadejpg <rafavenetikides@gmail.com>
2025-04-02 00:31:43 -0300

    FEAT: implemented other division operations and power

--- include/operations.h
+++ include/operations.h
@@ -14,8 +14,8 @@ void align_exp(FILE *outAsm);
 void op_sub_16bits(FILE *outAsm);
 void op_mult_16bits(FILE *outAsm);
 void op_div_16bits(FILE *outAsm);
-void op_integer_divide(FILE *outAsm);
-void op_modulo(FILE *outAsm);
+void op_div_int_16bits(FILE *outAsm);
+void op_rem_16bits(FILE *outAsm);
 void op_power(FILE *outAsm);
 
 #endif //OPERATIONS_H
